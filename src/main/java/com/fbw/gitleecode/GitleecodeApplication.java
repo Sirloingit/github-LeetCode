de510ec150b3e11dcf2ddf2909cd379a41195625
sirloin <19210300021@fudan.edu.cn>
2021-02-01 23:23:13 +0800

    initial springboot

---
+++ src/main/java/com/fbw/gitleecode/GitleecodeApplication.java
@@ -0,0 +1,13 @@
+package com.fbw.gitleecode;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitleecodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitleecodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fbw/gitleecode/GitleecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbw.gitleecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitleecodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
