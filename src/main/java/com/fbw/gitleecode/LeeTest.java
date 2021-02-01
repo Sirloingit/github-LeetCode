4e31f6b885018ad606e70748003f3a67d8c201cc
sirloin <19210300021@fudan.edu.cn>
2021-02-01 23:26:13 +0800

    test1

---
+++ src/main/java/com/fbw/gitleecode/LeeTest.java
@@ -0,0 +1,16 @@
+package com.fbw.gitleecode;
+
+import org.junit.Test;
+
+/**
+ * @author Sirloin
+ * @Date 2021-02-2021/2/1-23:24
+ */
+public class LeeTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
